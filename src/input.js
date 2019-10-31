0efc03abdc964502b01998a6b21995878e4f95cc
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-10-31 11:28:55 -0400

    Get user function complete

---
+++ src/input.js
@@ -0,0 +1,6 @@
+export function getUserInput(){
+    document.getElementById('user_submit').addEventListener('click', () => {
+        const userInput = document.getElementById('user_input');
+        return userInput;
+    })
+}
