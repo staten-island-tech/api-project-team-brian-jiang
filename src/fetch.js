4a9d181dde7bcbcbc1579eb001f4c5a688c5d3f6
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-11-06 10:42:36 -0500

    Possible CORS fix

--- src/fetch.js
+++ src/fetch.js
@@ -1,6 +1,6 @@
 export async function giantBombInit(){
   try{
-    const data = await fetch('https://www.giantbomb.com/api/characters/?api_key=9d667b02088703888d8e5b8b79705908424b1a0b');
+    const data = await fetch('https://crossorigin.me/https://www.giantbomb.com/api/characters/?api_key=9d667b02088703888d8e5b8b79705908424b1a0b');
     const result = await data.json();
     console.log(result);
   }
