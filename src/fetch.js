24f1204950356ad8c09c3e32e6223806c87138f8
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-11-06 11:02:31 -0500

    Update fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -1,7 +1,8 @@
 export async function giantBombInit(){
   try{
     const data = await fetch('https://www.giantbomb.com/api/character/1/?api_key=9d667b02088703888d8e5b8b79705908424b1a0b',{
-      mode: 'no-cors'
+      mode: 'no-cors',
+      body: JSON.stringify(data)
     });
     const result = await data.json();
     console.log(result);
