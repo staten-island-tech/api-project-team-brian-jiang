4ba03dc55dca466c07f42d9dfbc3bd1bfddd6061
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-11-06 10:54:24 -0500

    Permanent no-cors fix

--- src/fetch.js
+++ src/fetch.js
@@ -1,6 +1,8 @@
 export async function giantBombInit(){
   try{
-    const data = await fetch('https://crossorigin.me/https://www.giantbomb.com/api/characters/?api_key=9d667b02088703888d8e5b8b79705908424b1a0b');
+    const data = await fetch('https://www.giantbomb.com/api/character/1/?api_key=9d667b02088703888d8e5b8b79705908424b1a0b',{
+      mode: 'no-cors'
+    });
     const result = await data.json();
     console.log(result);
   }
