8bdded0de6bf898d9ee389cde86b036bc316c68f
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-11-06 11:35:21 -0500

    Still doesnt work

--- src/fetch.js
+++ src/fetch.js
@@ -1,13 +1,13 @@
 export async function giantBombInit(){
   try{
-    const data = await fetch('https://www.giantbomb.com/api/character/1/?api_key=9d667b02088703888d8e5b8b79705908424b1a0b',{
-      mode: 'no-cors',
-      body: JSON.stringify(data)
+    let data = await fetch('https://pokeapi.co/api/v2/pokemon/ditto',{
+      mode: 'no-cors'
     });
-    const result = await data.json();
+    let result = await data.json();
     console.log(result);
-  }
-  catch(err){
+  }catch(err){
     console.log(err)
   }
 }
+
+//https://www.giantbomb.com/api/character/1/?api_key=9d667b02088703888d8e5b8b79705908424b1a0b
