d480dcc0b624c08fbc1fcc3f724802c84d08bbdf
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-11-11 08:45:48 -0500

    Delete input.js

--- src/input.js
+++
@@ -1,6 +0,0 @@
-export function getUserInput(){
-    document.getElementById('submit').addEventListener('click', () => {
-        const userInput = document.getElementById('user_input').value;
-        console.log (userInput);
-    })
-}
