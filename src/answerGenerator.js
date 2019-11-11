8199ae6d74260616fd352b2eadb6cf4c1644a7a4
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-11-11 12:58:50 -0500

    Undefined answer catch

--- src/answerGenerator.js
+++ src/answerGenerator.js
@@ -7,15 +7,19 @@ export async function answerGenerator(qType, maxCount, data){
         game = game.results.name;
         return game;
     }
+    async function answerCatch(ans){
+        if(ans == undefined){
+            ans = await generator();
+            answerCatch(ans);
+        }
+    }
     let a1 = data[Math.floor(Math.random() * data.length)].name;
     let a2 = await generator();
     let a3 = await generator();
     let a4 = await generator();
     let aArray = [a1, a2, a3, a4];
-    aArray.forEach(async function(el){
-        if(el == undefined){
-            el = await generator();
-        }
+    aArray = aArray.map(el => {
+        answerCatch(el);
     });
     aArray = randomizer(aArray);
     Array.from(document.querySelectorAll('.trivia_answer_choice')).map(function(el, i){
