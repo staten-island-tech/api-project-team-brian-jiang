6667b918b4c7666dea3f25f4ea12452653701732
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-11-11 13:38:38 -0500

    Fixed undefined answer catch

--- src/answerGenerator.js
+++ src/answerGenerator.js
@@ -4,13 +4,12 @@ export async function answerGenerator(qType, maxCount, data){
     async function generator(){
         let game = await fetch(`https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/${qType}/${Math.floor(Math.random() * maxCount-1)}/?api_key=ba31dd109b521b66607178ada5c6b8f4fc13ca0b&field_list=name&format=json`);
         game = await game.json();
+        console.log(game);
+        if(game.status_code != 1){
+            return await generator();
+        }else{
         game = game.results.name;
         return game;
-    }
-    async function answerCatch(ans){
-        if(ans == undefined){
-            ans = await generator();
-            answerCatch(ans);
         }
     }
     let a1 = data[Math.floor(Math.random() * data.length)].name;
@@ -18,9 +17,6 @@ export async function answerGenerator(qType, maxCount, data){
     let a3 = await generator();
     let a4 = await generator();
     let aArray = [a1, a2, a3, a4];
-    aArray = aArray.map(el => {
-        answerCatch(el);
-    });
     aArray = randomizer(aArray);
     Array.from(document.querySelectorAll('.trivia_answer_choice')).map(function(el, i){
         el.innerHTML = aArray[i]
